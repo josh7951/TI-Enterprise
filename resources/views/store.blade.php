f95f3332b7f96ea890d24805f3f73283518acee3
Joshua Mejia <jmejia7951@gmail.com>
2020-02-08 12:00:15 -0800

    Added Hero Image for Bait Store

--- resources/views/store.blade.php
+++ resources/views/store.blade.php
@@ -7,6 +7,14 @@
     <li class="is-active"><a href="#" aria-current="page">Bait Store</a></li>
   </ul>
 </nav>
-<h1>Num Nums for Fishy Wishy Yummy Tummy</h1>
+<section class="hero is-medium is-primary is-bold" id="store-hero">
+  <!-- <img class="image" title="Bait Store" src="\images\store1.jpg" alt="store1.jpg"> -->
+  <div class="hero-body">
+      <div class="container" style="text-align:center;">
+        <h1 class="title is-3">Bait Store</h1>
+        <h2 class="subtitle">Num Nums for Fishy Wishy Yummy Tummy</h2>
+      </div>
+  </div>
+</section>
 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci quia, vitae reiciendis tempora nulla perspiciatis quo fuga ab voluptatibus culpa, quaerat minus. Amet asperiores veritatis facilis sunt voluptatibus vitae ex.</p>
 @endsection
