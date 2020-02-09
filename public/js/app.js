04abb88bb26f8b15aeef0a20a931d30ae6e81fa9
Raven Thio <thior@uci.edu>
2020-02-08 20:21:56 -0800

    test

--- public/js/app.js
+++ public/js/app.js
@@ -49804,8 +49804,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\jmeji\Documents\GitHub\TI-Enterprise\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\jmeji\Documents\GitHub\TI-Enterprise\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\Users\Raven\Projects\TI-Enterprise\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\Raven\Projects\TI-Enterprise\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
