de5a4a20868879e1d8fb4cc0d9cb4d8d03a8f125
josh7951 <jmejia7951@gmail.com>
2020-02-08 20:44:08 -0800

    Expanded content on the store page

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -11,12 +11,28 @@
       </span>
     </div>
     <div id="navMenu" class="navbar-menu">
-      <div class="navbar-start">
+      <div class="navbar-end">
         <a href="/about" class="navbar-item">Our Team</a>
         <a href="/team-gallery" class="navbar-item">Gallery</a>
+        <a href="#" class="navbar-item">Tournaments</a>
         <a href="/store" class="navbar-item">Bait Store</a>
-        <a href="#" class="navbar-item">Contact Us</a>
-        
+        <div class="navbar-item has-dropdown is-hoverable">
+        <a class="navbar-link">Resources</a>
+        <div class="navbar-dropdown">
+          <a class="navbar-item" href="https://americanbass.com/" target="_blank">American Bass</a>
+          <a class="navbar-item" href="https://www.bassmaster.com/" target="_blank">Bass Master</a>
+          <a class="navbar-item" href="https://www.bassresource.com/" target="_blank">Bass Resource - Fishing Guide</a>
+          <a class="navbar-item" href="https://www.flwfishing.com/" target="_blank">Fishing League Worldwide</a>
+          <a class="navbar-item" href="https://www.gamakatsu.com/" target="_blank">Gamakatsu Hooks</a>
+          <a class="navbar-item" href="https://www.lakesideboatsinc.com/" target="_blank">Lakeside Boat & Marine</a>
+          <a class="navbar-item" href="https://www.ownerhooks.com/" target="_blank">Owner Hooks</a>
+          <a class="navbar-item" href="https://www.tacklewarehouse.com/" target="_blank">Tackle Warehouse</a>
+          <a class="navbar-item" href="https://tearshot.com/" target="_blank">Team Davies Tackles</a>
+          <a class="navbar-item" href="http://wonbass.com/" target="_blank">WON Bass Tournaments</a>
+          <hr class="navbar-divider">
+          <a class="navbar-item" href="/contact" style="color:#41CAE5; font-weight: bold;">Contact Us</a>
+        </div>
+      </div>
       </div>
     </div>
   </div>
