bc907e880098e95dbd871e9d621db60ddf36b8ca
Joshua Mejia <jmejia7951@gmail.com>
2021-06-10 00:14:15 -0700

    some component organizatioin

--- resources/js/app.js
+++ resources/js/app.js
@@ -19,15 +19,19 @@ window.Vue = require('vue').default;
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
+
 Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+/** Components used on brochure site */
 Vue.component('home-content', require('./components/HomeComponent.vue').default);
 Vue.component('team-content', require('./components/TeamComponent.vue').default);
 Vue.component('store-content', require('./components/StoreComponent.vue').default);
 Vue.component('contact-content', require('./components/Contact.vue').default);
+Vue.component('caldwell-content', require('./components/CaldwellComponent.vue').default);
 Vue.component('navbar', require('./components/Navbar.vue').default);
+
+/** Components for admin panel */
 Vue.component('dashbar', require('./components/NavbarDash.vue').default);
 Vue.component('aside-menu', require('./components/AsideMenu.vue').default);
-Vue.component('caldwell-content', require('./components/CaldwellComponent.vue').default);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
