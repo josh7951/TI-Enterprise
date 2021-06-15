a7f0b5c4fbd5b76fbefe335ef4848bdc8ed76ba5
Josh Mejia <jmejia7951@gmail.com>
2021-06-14 17:20:44 -0700

    created table view for tournament schedule

--- resources/views/admin/tournamentindex.blade.php
+++ resources/views/admin/tournamentindex.blade.php
@@ -1,5 +1,45 @@
 @extends('layouts.dash')
 
 @section('content')
-  Tournament Editor time
+  <div class="container has-text-centered">
+    <div class="columns is-mobile is-centered">
+      <div class="column">
+        <div>
+          <h1 class="title">Tournament Schedule</h1>
+          <hr>
+        </div>
+        <table class="table is-striped is-bordered">
+          <thead>
+            <tr>
+              <td>Series</td>
+              <td>Tournament</td>
+              <td>Location</td>
+              <td>Start Date</td>
+              <td>End Date</td>
+              <td colspan="2">Action</td>
+            </tr>
+          </thead>
+          <tbody>
+            @foreach($tournaments as $tournament)
+            <tr>
+              <td>{{$tournament->series}}</td>
+              <td>{{$tournament->tournament}}</td>
+              <td>{{$tournament->location}}</td>
+              <td>{{$tournament->start_date}}</td>
+              <td>{{$tournament->end_date}}</td>
+              <td><a href="{{ route('tournament-editor.edit', $tournament->id)}}" class="btn btn-primary">Edit</a></td>
+              <td>
+                  <form action="{{ route('tournament-editor.destroy', $tournament->id)}}" method="post">
+                    @csrf
+                    @method('DELETE')
+                    <button class="btn btn-danger" type="submit">Delete</button>
+                  </form>
+              </td>
+            </tr>
+            @endforeach
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
 @endsection
